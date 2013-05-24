725cbe0a519a5134e8743bca64070cbe563dfc60
chris <chris@coronis>
2013-05-23 21:08:20 -0400

    Added setup.py for standard install. Updated to work with django-cms 2.4 and django 1.5.

---
+++ cmsplugin_rst/migrations/0001_initial.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ReSTField'
+        db.create_table(u'cmsplugin_restfield', (
+            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
+            ('rst', self.gf('django.db.models.fields.TextField')()),
+            ('rst_html', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'cmsplugin_rst', ['ReSTField'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ReSTField'
+        db.delete_table(u'cmsplugin_restfield')
+
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        u'cmsplugin_rst.restfield': {
+            'Meta': {'object_name': 'ReSTField', 'db_table': "u'cmsplugin_restfield'", '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'rst': ('django.db.models.fields.TextField', [], {}),
+            'rst_html': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['cmsplugin_rst']
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+import urllib
+
+from setuptools import setup, find_packages, Command
+
+from cmsplugin_rst import __version__
+
+setup(
+    name = "cmsplugin_rst",
+    version = __version__,
+    #packages = find_packages(),
+    author = "Chris Spencer",
+    author_email = "chrisspen@gmail.com",
+    description = "A django-cms plugin for supporting Restructured Text.",
+    license = "BSD",
+    url = "https://github.com/chrisspen/cmsplugin_rst",
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires = [
+        'django-cms>=2.0.0',
+    ],
+)
